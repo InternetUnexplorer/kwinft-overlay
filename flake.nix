5d165dd5dbe0e8ff2a12e523dea233d4ea2c8be5
InternetUnexplorer <internetunexplorer@gmail.com>
2022-10-27 21:51:35 -0700

    Fix flake.nix

--- flake.nix
+++ flake.nix
@@ -1,4 +1,4 @@
 {
   description = "Overlay for KWinFT/Disman/KDisplay/Wrapland";
-  outputs = { self }: { overlays.default = [ (import ./default.nix) ]; };
+  outputs = { self }: { overlays.default = import ./.; };
 }
